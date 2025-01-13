0d2676660340512b716abea90db020fb65c02dcc
= <=>
2025-01-13 20:08:42 -0300

    .

--- rodarprograma.py
+++ rodarprograma.py
@@ -3,10 +3,10 @@ import os
 
 comando_base = 'teste.exe'
 metodo = str(2)
-quantidade = str(200000)
+quantidade = str(200)
 ordem = str(3)
 chave = str(67)
-imprimir = ''
+imprimir = '-P'
 
 subprocess.run(['gcc', '*.c', '-o', comando_base])
 subprocess.run([comando_base, metodo, quantidade, ordem, chave, imprimir])
